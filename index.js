8c037ffd8a6eb014531a97920307fcea990a3c25
arikmsn <75578477+arikmsn@users.noreply.github.com>
2026-02-06 01:23:30 +0200

    Change string format
    Updated API key and token to use string literals.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 
 // מפתחות - כאן מדביקים את מה שהעתקנו
-const GEMINI_API_KEY = AIzaSyB_zaUu7dK-ugcCXRMIdM18HRWsu5FdZhM;
-const WHATSAPP_TOKEN = EAAZBE0LZA46GwBQiKr5gacQ5rgBypSN4AylRuJLvbZCo9IsO5tKjFM05PDg3fZAxp9Nr97JJpYoy4YF7py3lUXXJ6ZAV51eZBSMYBkh0qP58s3GM1ZA3QWZBCo1SnDMoZA2NEmFrWuYet5cBbkEVIIjz0NthLU5cfkk89o4mVgCBNJblGxsIOJRGMq6HsfG2VolZBiAgpTjeQy8JglfhAg13SiujAF2KY9KJIr1tFQQHd1NUAQexYQzZBDOPahmWLUvT53zLrNwt87XwIY7kvzzkgZDZD;
+const GEMINI_API_KEY = "AIzaSyB_zaUu7dK-ugcCXRMIdM18HRWsu5FdZhM";
+const WHATSAPP_TOKEN = "EAAZBE0LZA46GwBQiKr5gacQ5rgBypSN4AylRuJLvbZCo9IsO5tKjFM05PDg3fZAxp9Nr97JJpYoy4YF7py3lUXXJ6ZAV51eZBSMYBkh0qP58s3GM1ZA3QWZBCo1SnDMoZA2NEmFrWuYet5cBbkEVIIjz0NthLU5cfkk89o4mVgCBNJblGxsIOJRGMq6HsfG2VolZBiAgpTjeQy8JglfhAg13SiujAF2KY9KJIr1tFQQHd1NUAQexYQzZBDOPahmWLUvT53zLrNwt87XwIY7kvzzkgZDZD";
 
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
